152f4a4f2f05f92e8a7c3260b9ca577cd8c42750
Benjamin Ebner <ebner.benjamin@gmail.com>
2021-07-23 15:11:55 +0200

    End of mission 2

--- src/config/config.js
+++ src/config/config.js
@@ -3,4 +3,5 @@ import { config } from "@onflow/fcl";
 config({
   "accessNode.api": process.env.REACT_APP_ACCESS_NODE,
   "discovery.wallet": process.env.REACT_APP_WALLET_DISCOVERY,
+  "0xDappy": process.env.REACT_APP_DAPPY_CONTRACT,
 });
--- src/hooks/use-dappy-templates.hook.js
+++ src/hooks/use-dappy-templates.hook.js
@@ -1,26 +1,34 @@
-import { useEffect, useReducer } from 'react'
-import { defaultReducer } from '../reducer/defaultReducer'
-import DappyClass from '../utils/DappyClass'
-import { generateDappies } from '../utils/dappies.utils'
+import { useEffect, useReducer } from "react";
+import { query } from "@onflow/fcl";
+
+import { defaultReducer } from "../reducer/defaultReducer";
+import DappyClass from "../utils/DappyClass";
+import { LIST_DAPPY_TEMPLATES } from "../flow/list-dappy-templates.script";
 
 export default function useDappyTemplates() {
-  const [state, dispatch] = useReducer(defaultReducer, { loading: false, error: false, data: [] })
+  const [state, dispatch] = useReducer(defaultReducer, {
+    loading: false,
+    error: false,
+    data: [],
+  });
 
   useEffect(() => {
     const fetchDappyTemplates = async () => {
-      dispatch({ type: 'PROCESSING' })
+      dispatch({ type: "PROCESSING" });
       try {
-        let res = generateDappies()
-        let mappedDappies = Object.values(res).map(d => {
-          return new DappyClass(d?.templateID, d?.dna, d?.name, d?.price)
-        })
-        dispatch({ type: 'SUCCESS', payload: mappedDappies })
+        let res = await query({
+          cadence: LIST_DAPPY_TEMPLATES,
+        });
+        let mappedDappies = Object.values(res).map((d) => {
+          return new DappyClass(d?.templateID, d?.dna, d?.name, d?.price);
+        });
+        dispatch({ type: "SUCCESS", payload: mappedDappies });
       } catch (err) {
-        dispatch({ type: 'ERROR' })
+        dispatch({ type: "ERROR" });
       }
-    }
-    fetchDappyTemplates()
-  }, [])
+    };
+    fetchDappyTemplates();
+  }, []);
 
-  return state
+  return state;
 }
