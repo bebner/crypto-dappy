be3d8e33dbf20fb3c16c3c264cd952ed52b0b8af
Nguyen Dang Le <nguyenivan@hotmail.com>
2021-10-28 16:02:58 +0700

    Nothing

--- up.sh
+++ up.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-flow accounts remove-contract GalleryContract --network testnet --signer MyWorldAdmin
+flow accounts remove-contract GalleryContract --network testnet --signer MyAdmin
 
-flow accounts remove-contract NonFungibleToken  --network testnet --signer MyWorldAdmin
+flow accounts remove-contract NonFungibleToken  --network testnet --signer MyAdmin
 
-flow accounts remove-contract DappyNFT  --network testnet --signer MyWorldAdmin
+flow accounts remove-contract DappyNFT  --network testnet --signer MyAdmin
 
-flow accounts remove-contract PackNFT  --network testnet --signer MyWorldAdmin
+flow accounts remove-contract PackNFT  --network testnet --signer MyAdmin
 
-flow accounts remove-contract NFTStorefront  --network testnet --signer MyWorldAdmin
+flow accounts remove-contract NFTStorefront  --network testnet --signer MyAdmin
 
-flow accounts remove-contract DappyContract  --network testnet --signer MyWorldAdmin
+flow accounts remove-contract DappyContract  --network testnet --signer MyAdmin
 
 flow project deploy --network testnet
 
-flow transactions send cadence/transactions/CreateAdminGallery.cdc --signer MyWorldAdmin --network testnet
+flow transactions send cadence/transactions/CreateAdminGallery.cdc --signer MyAdmin --network testnet
 
-flow transactions send cadence/transactions/PrepareDappyContract.cdc --signer MyWorldAdmin --network testnet
+flow transactions send cadence/transactions/PrepareDappyContract.cdc --signer MyAdmin --network testnet
