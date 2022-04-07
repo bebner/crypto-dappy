38d920b2f22af0cdc2dea464dfb379f34da54e58
TraXanh13 <kyleduong13@gmail.com>
2022-04-07 13:15:22 -0600

    Completed mission 1 (Authen wallet)

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ import {config} from "@onflow/fcl";
 
 config({
     "accessNode.api": process.env.REACT_APP_ACCESSNODE,
-    "discovery.wallet": process.env.REACT_APP_WALLET_DISCOVERY
+    "discovery.wallet": process.env.REACT_APP_WALLET_DISCOVERY,
 })
